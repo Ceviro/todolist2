c19195f5cee475a2a067a0ec5734bfd47ebdb78f
Ceviro <ceviroho@gmail.com>
2023-10-22 19:19:01 +0700

    Update: describe your changes here

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,25 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+
+ document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.check-box').forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+      var togglePath = checkbox.dataset.togglePath;
+      var csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+      fetch(togglePath, {
+        method: 'PATCH',
+        headers: {
+          'X-CSRF-Token': csrfToken,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => response.ok ? response.json() : Promise.reject('Request failed'))
+      .then(data => {
+      })
+      .catch(error => console.error('AJAX error:', error));
+    });
+  });
+});
+
